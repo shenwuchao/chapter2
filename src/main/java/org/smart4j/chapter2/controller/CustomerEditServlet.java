e3af13e4e35b79c2315d672b73783359a5f28136
shenwuchao <wbj_yt@163.com>
2018-06-02 20:35:18 +0800

    客户编辑servlet

---
+++ src/main/java/org/smart4j/chapter2/controller/CustomerEditServlet.java
@@ -0,0 +1,27 @@
+package org.smart4j.chapter2.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/customer_edit")
+public class CustomerEditServlet extends HttpServlet {
+    /*
+    * 进入 客户编辑 页面
+    * */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    /*
+    * 编辑客户
+    * */
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
